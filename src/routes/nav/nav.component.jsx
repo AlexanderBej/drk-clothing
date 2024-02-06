a6a36623f4725b50f76e7a729375b216d0960846
Alexandru Bej <alexbej07@gmail.com>
2024-02-06 22:32:29 +0200

    added sign in, firebase auth and firebase db

---
+++ src/routes/nav/nav.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import { ReactComponent as DrkLogo } from './../../assets/crown.svg';
+import './nav.styles.scss';
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <nav className="nav-bar">
+                <Link className="logo-container" to='/'>
+                    <DrkLogo className="logo" />
+                </Link>
+                <div className="links-container">
+                    <Link to='/shop' className="nav-link">
+                        SHOP
+                    </Link>
+                    <Link to='/sign-in' className="nav-link">
+                        SIGN IN
+                    </Link>
+                </div>
+            </nav>
+            <Outlet />
+        </Fragment>
+    );
+}
+
+export default Navigation;
---
+++ src/utils/firebase/firebase.utils.jsx
@@ -0,0 +1,59 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { 
+    getAuth, 
+    signInWithRedirect, 
+    signInWithPopup, 
+    GoogleAuthProvider 
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBTufbKet5b7FH9UUitwwOYuiPti_FI-t4",
+    authDomain: "drk-clothing-db.firebaseapp.com",
+    projectId: "drk-clothing-db",
+    storageBucket: "drk-clothing-db.appspot.com",
+    messagingSenderId: "870258282632",
+    appId: "1:870258282632:web:0d79726867bc9dd0904f3d"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+}
