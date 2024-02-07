93644e5e9123eb87599a013fe9d79416ce57da4e
Alexandru Bej <alexbej07@gmail.com>
2024-02-07 18:56:55 +0200

    added cart icon and dropdown, products, and the shop page

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -3,18 +3,18 @@ import { createContext, useEffect, useState } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
-    currentUser:null,
+    currentUser: null,
     setCurrentUser: () => null,
 });
 
-export const UserProvider = ({children}) => {
+export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
-    const value = {currentUser, setCurrentUser};
+    const value = { currentUser, setCurrentUser };
 
     useEffect(() => {
         const unsubscribe = onAuthStateChangedListener((user) => {
             if (user) {
-                createUserDocumentFromAuth(user);  
+                createUserDocumentFromAuth(user);
             }
             setCurrentUser(user);
             console.log(user)
--- src/routes/nav/nav.component.jsx
+++ src/routes/nav/nav.component.jsx
@@ -2,13 +2,18 @@ import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { ReactComponent as DrkLogo } from './../../assets/crown.svg';
 import './nav.styles.scss';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const {isCartOpen} = useContext(CartContext);
 
     return (
         <Fragment>
@@ -29,7 +34,9 @@ const Navigation = () => {
                             </Link>
                         )
                     }
+                    <CartIcon />
                 </div>
+                {isCartOpen && <CartDropdown />}
             </nav>
             <Outlet />
         </Fragment>
